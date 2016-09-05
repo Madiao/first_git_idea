1cf60f0591f9ec8ae297065432105a4c35aa12f9
madiao <madiao@foxmail.com>
2016-09-05 22:46:54 +0800

    create git_test

---
+++ src/main/java/madiao/gittest/HelloWorld.java
@@ -0,0 +1,10 @@
+package madiao.gittest;
+
+/**
+ * Created by madiao on 2016/9/5.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
